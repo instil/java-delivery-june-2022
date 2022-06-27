78056c2c3f4c13d773c377d800452b3f3698ff0c
Garth.Gilmour <garth.gilmour@instil.co>
2022-06-27 16:03:24 +0100

    Add Videostore solution

--- exercises/Language/src/main/java/videostore/Customer.java
+++ exercises/Language/src/main/java/videostore/Customer.java
@@ -6,8 +6,9 @@ package videostore;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-import static videostore.PriceCode.NEW_RELEASE;
+import static java.lang.String.format;
 
 public class Customer {
     private String name;
@@ -25,56 +26,43 @@ public class Customer {
         rentals.add(arg);
     }
 
-    public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
-        StringBuilder result = new StringBuilder();
-        //add header lines
-        result.append("\nRental Record for ");
-        result.append(getName());
-        result.append("\n");
-        for (Rental each : rentals) {
-            double thisAmount = 0;
-            //determine amounts for each line
-            switch (each.getMovie().getPriceCode()) {
-                case REGULAR:
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2)
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    break;
-                case NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-                case CHILDRENS:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    break;
-            }
-            // add frequent renter points
-            frequentRenterPoints++;
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == NEW_RELEASE)
-                    && each.getDaysRented() > 1)
-                frequentRenterPoints++;
-            //show figures for this rental
-            result.append("\t");
-            result.append(each.getDaysRented());
-            result.append("\t");
-            result.append(each.getMovie().getTitle());
-            result.append("\t");
-            result.append(thisAmount);
-            result.append("\n");
-            //add cost of this rental to total cost
-            totalAmount += thisAmount;
-        }
-        //add footer lines
-        result.append("Amount owed is ");
-        result.append(totalAmount);
-        result.append("\n");
-        result.append("You earned ");
-        result.append(frequentRenterPoints);
-        result.append(" frequent renter points\n");
-        return result.toString();
+    private double totalAmount() {
+        //add cost of this rental to total cost
+        return rentals
+                .stream()
+                .mapToDouble(Rental::calcAmount)
+                .sum();
     }
+
+    private int totalPoints() {
+        return rentals
+                .stream()
+                .mapToInt(Rental::calcPoints)
+                .sum();
+    }
+
+    public String statement() {
+        var header = format("\nRental Record for %s\n", getName());
+        var rentalLines = rentalLines();
+        var footer1 = format("Amount owed is %.2f\n", totalAmount());
+        var footer2 = format("You earned %d frequent renter points\n", totalPoints());
+
+        return header + rentalLines + footer1 + footer2;
+    }
+
+    private String rentalLines() {
+        return rentals
+                .stream()
+                .map(this::formatLine)
+                .collect(Collectors.joining());
+    }
+
+    private String formatLine(Rental each) {
+        return format(
+                "\t%d\t%s\t%.2f\n",
+                each.getDaysRented(),
+                each.getMovie().getTitle(),
+                each.calcAmount());
+    }
+
 }
--- exercises/Language/src/main/java/videostore/Rental.java
+++ exercises/Language/src/main/java/videostore/Rental.java
@@ -4,6 +4,8 @@
  */
 package videostore;
 
+import static videostore.PriceCode.NEW_RELEASE;
+
 public class Rental {
     private Movie movie;
     private int daysRented;
@@ -19,5 +21,33 @@ public class Rental {
 
     public Movie getMovie() {
         return movie;
+    }
+
+    double calcAmount() {
+        double amount = 0;
+        switch (getMovie().getPriceCode()) {
+            case REGULAR:
+                amount += 2;
+                if (getDaysRented() > 2)
+                    amount += (getDaysRented() - 2) * 1.5;
+                break;
+            case NEW_RELEASE:
+                amount += getDaysRented() * 3;
+                break;
+            case CHILDRENS:
+                amount += 1.5;
+                if (getDaysRented() > 3)
+                    amount += (getDaysRented() - 3) * 1.5;
+                break;
+        }
+        return amount;
+    }
+
+    int calcPoints() {
+        // add bonus for a two day new release rental
+        if ((getMovie().getPriceCode() == NEW_RELEASE)
+                && getDaysRented() > 1)
+            return 2;
+        return 1;
     }
 }
