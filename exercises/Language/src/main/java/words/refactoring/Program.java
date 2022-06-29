124c15f5b98f230775f11af55698072077a064e9
Garth.Gilmour <garth.gilmour@instil.co>
2022-06-29 10:49:51 +0100

    Add solution to words refactoring

--- exercises/Language/src/main/java/words/refactoring/Program.java
+++ exercises/Language/src/main/java/words/refactoring/Program.java
@@ -11,27 +11,73 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Program {
+
+    public static final String FILE_NAME = "./data/uniqueWords.txt";
+
     public static void main(String[] args) throws IOException {
-        Path path = new File("./data/uniqueWords.txt").toPath();
-        Stream<String> lines = Files.lines(path);
-        Map<String, List<String>> tabularResults = lines.map(line -> line.split("\\s+"))
-                .flatMap(Arrays::stream)
-                .map(String::toLowerCase)
-                .map(str -> str.replaceAll("\\W", ""))
-                .filter(str -> !str.matches("^[0-9]+$"))
-                .collect(Collectors.groupingBy(word -> word));
+        readLinesFromFile().ifPresentOrElse(
+                Program::printWordsByFrequency,
+                Program::handleError
+        );
+    }
 
-        List<Pair<String, Integer>> listResults = new ArrayList<>();
-        tabularResults.forEach((word, instances) -> {
-            if(instances.size() > 1) {
-                listResults.add(new Pair<>(word, instances.size()));
-            }
-        });
-        listResults.sort(Comparator.comparingInt(Pair::getSecond));
-        Collections.reverse(listResults);
+    private static void handleError() {
+        System.out.println("Cannot read from " + FILE_NAME);
+    }
 
-        listResults.stream()
+    private static void printWordsByFrequency(Stream<String> lines) {
+        Map<String, List<String>> tableOfWords = tabulateWords(lines);
+
+        List<Pair<String, Integer>> wordPairs = buildWordPairs(tableOfWords);
+
+        printWordPairs(wordPairs);
+    }
+
+    private static void printWordPairs(List<Pair<String, Integer>> wordPairs) {
+        wordPairs.stream()
                 .map(pair -> String.format("%s %d", pair.getFirst(), pair.getSecond()))
                 .forEach(System.out::println);
+    }
+
+    private static List<Pair<String, Integer>> buildWordPairs(Map<String, List<String>> tabularResults) {
+        List<Pair<String, Integer>> results = tabularResults
+                .entrySet()
+                .stream()
+                .map(entry -> new Pair<>(entry.getKey(), entry.getValue().size()))
+                .filter(pair -> pair.getSecond() > 1)
+                .sorted(Comparator.comparingInt(Pair::getSecond))
+                .collect(Collectors.toList());
+
+        Collections.reverse(results);
+        return results;
+    }
+
+    private static Map<String, List<String>> tabulateWords(Stream<String> lines) {
+        return lines
+                .flatMap(Program::lineToWords)
+                .map(Program::normaliseWord)
+                .filter(Program::isNotNumber)
+                .collect(Collectors.groupingBy(word -> word));
+    }
+
+    private static boolean isNotNumber(String word) {
+        return !word.matches("^[0-9]+$");
+    }
+
+    private static String normaliseWord(String word) {
+        return word.toLowerCase().replaceAll("\\W", "");
+    }
+
+    private static Stream<String> lineToWords(String line) {
+        return Stream.of(line.split("\\s+"));
+    }
+
+    private static Optional<Stream<String>> readLinesFromFile() {
+        Path path = new File(FILE_NAME).toPath();
+        try {
+            return Optional.of(Files.lines(path));
+        } catch (IOException ex) {
+            return Optional.empty();
+        }
     }
 }
